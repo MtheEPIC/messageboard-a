c7b8e0a75c7b55d49390aac2a461f9b99e636039
MtheEPIC <mtheepic1423@protonmail.com>
2021-03-21 17:58:45 +0200

    basic function skeleton

---
+++ Board.cpp
@@ -0,0 +1,33 @@
+#include "Board.hpp"
+#include "Direction.hpp"
+using ariel::Direction;
+
+#include <iostream>
+#include <stdexcept>
+
+using namespace std;
+using namespace ariel;
+
+namespace ariel {
+	Board::Board()
+	{
+		
+	}
+	Board::~Board()
+	{
+		
+	}
+	void Board::post(UI x, UI y, Direction dir, std::string msg)
+	{
+		
+	}
+	std::string Board::read(UI x, UI y, ariel::Direction dir, UI length)
+	{
+		return " ";
+	}
+	void Board::show()
+	{
+		
+	}
+}
+
