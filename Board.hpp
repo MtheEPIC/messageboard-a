67eefcb170a91caf3df20e6e8ed320f776a85f0b
MtheEPIC <mtheepic1423@protonmail.com>
2021-03-21 19:23:05 +0200

    moved func to public scope for tests

--- Board.hpp
+++ Board.hpp
@@ -10,15 +10,17 @@ namespace ariel {
 	{
 	public:
 		Board();
+		Board(UI, UI);
 		~Board();
 		void post(UI, UI, ariel::Direction, std::string);
 		std::string read(UI, UI, ariel::Direction, UI);
 		void show();
-	private:
+		
+		// public only for tests
 		inline UI get_rows() {return this->_ROWS;}
 		inline UI get_cols() {return this->_COLS;}
 		inline char **get_board() {return this->_board;}
-		
+	private:
 		UI _x, _y, _length, _ROWS, _COLS;
 		std::string _msg;
 		ariel::Direction _direction;
