900691982054960abf135a11bc153452ab1c4b48
MtheEPIC <mtheepic1423@protonmail.com>
2021-03-21 17:59:33 +0200

    basic file structure

---
+++ Test.cpp
@@ -0,0 +1,19 @@
+#include "doctest.h"
+#include "Board.hpp"
+#include <string.h>
+
+using namespace ariel;
+using namespace std;
+
+TEST_CASE("Post")
+{
+	CHECK(1==1);
+}
+TEST_CASE("Read")
+{
+	CHECK(1==1);
+}
+TEST_CASE("Show")
+{
+	CHECK(1==1);
+}
