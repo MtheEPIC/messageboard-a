c4d14d02d9c6c1ce6cfef5976b6f5c1197ab5839
MtheEPIC <mtheepic1423@protonmail.com>
2021-03-21 20:56:49 +0200

    basic tests

--- Test.cpp
+++ Test.cpp
@@ -5,15 +5,61 @@
 using namespace ariel;
 using namespace std;
 
+TEST_CASE("Bad Input")
+{
+	CHECK_THROWS(new Board(0, 0));
+	CHECK_THROWS(new Board(1, 0));
+	CHECK_THROWS(new Board(0, 1));
+}
+TEST_CASE("Resize Board")
+{
+	Board br(1, 1);
+	br.post(0, 0, Direction::Horizontal, "hello");
+	CHECK(br.get_cols()==7); // if msg>size, new size = (size + size_of(msg) - free space) * 1.5
+	br.post(0, 0, Direction::Vertical, "hello");
+	CHECK(br.get_rows()==7);
+}
 TEST_CASE("Post")
 {
-	CHECK(1==1);
+	Board br;
+	CHECK_THROWS(br.post(0, 0, Direction::Horizontal, ""));
 }
 TEST_CASE("Read")
 {
-	CHECK(1==1);
+	Board br;
+	CHECK_THROWS(br.read(0, 0, Direction::Horizontal, 0));
+	CHECK(br.read(0, 0, Direction::Horizontal, 5).size() == 0);
+	
+	br.post(0, 0, Direction::Horizontal, "string with length of 24");
+	CHECK_THROWS(br.read(0, 0, Direction::Horizontal, 0));
+	CHECK_THROWS(br.read(0, 0, Direction::Horizontal, 25));
+	CHECK(br.read(0, 0, Direction::Horizontal, 24).size() == 24);
+	CHECK(br.read(0, 0, Direction::Horizontal, 5).size() == 5);
 }
 TEST_CASE("Show")
 {
-	CHECK(1==1);
+	Board br;
+	CHECK(br.show() == "");
+	
+	br.post(0, 0, Direction::Horizontal, "abc");
+	CHECK(br.show() == "abc");
+	
+	br.post(0, 0, Direction::Vertical, "abc");
+	CHECK(br.show() == "abc\nb--\nc--");
+	
+	br.post(0, 1, Direction::Vertical, "abc");
+	br.post(0, 2, Direction::Vertical, "abc");
+	CHECK(br.show() == "abc\nbbb\nccc");
+	
+	br.post(1, 0, Direction::Horizontal, "abc");
+	br.post(2, 0, Direction::Horizontal, "abc");
+	CHECK(br.show() == "abc\nabc\nabc");
+	
+	Board br1;
+	br.post(1, 1, Direction::Horizontal, "abc");
+	CHECK(br1.show() == "----\n-abc");
+	
+	Board br2;
+	br2.post(1, 1, Direction::Vertical, "abc");
+	CHECK(br2.show() == "--\n-a\n-b\n-c");
 }
