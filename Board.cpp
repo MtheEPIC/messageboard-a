50045cbd3c454fd74fa6ae5de0439206d321e497
MtheEPIC <mtheepic1423@protonmail.com>
2021-03-22 13:35:13 +0200

    exceptions

--- Board.cpp
+++ Board.cpp
@@ -12,7 +12,7 @@ namespace ariel {
 	{
 		
 	}
-	Board::Board(UI rows, UI cols)
+	Board::Board(CUI rows, CUI cols)
 	{
 		if (rows == 0 || cols == 0)
 		{
@@ -23,15 +23,37 @@ namespace ariel {
 	{
 		
 	}
-	void Board::post(UI x, UI y, Direction dir, std::string msg)
+	void Board::post(CUI x, CUI y, CDir dir, Cstr msg)
 	{
-		
+		if (msg.length() == 0)
+		{
+			throw std::invalid_argument("message must not be empty");
+		}
 	}
-	string Board::read(UI x, UI y, ariel::Direction dir, UI length)
+	Cstr Board::read(CUI x, CUI y, CDir dir, CUI length)
 	{
+		if (length == 0)
+		{
+			throw std::invalid_argument("length must more then 0");
+		}
+		switch(dir)
+		{
+		case Direction::Horizontal:
+			if (this->get_cols() < length)
+			{
+				throw std::out_of_range("length var is to long");
+			}
+			break;
+		case Direction::Vertical:
+			if (this->get_rows() < length)
+			{
+				throw std::out_of_range("length var is to long");
+			}
+			break;
+		}
 		return "";
 	}
-	string Board::show()
+	Cstr Board::show()
 	{
 		return "";
 	}
